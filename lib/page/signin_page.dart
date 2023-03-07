c938441a66ad70eab74d6eda1c39795d1e952b47
RustamovOdilbek <odilbekrustamov1215@gmail.com>
2023-03-07 17:26:23 +0500

    Initial commit

---
+++ lib/page/signin_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SigninPage extends StatefulWidget {
+  static final String id = "signin_page";
+
+  @override
+  State<SigninPage> createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
---
+++ lib/page/signup_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  static final String id = "signup_page";
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
